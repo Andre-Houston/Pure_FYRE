5773478be6b7eac2ee05fa452f6df7743896d750
Andre Houston <houston.gamedev@gmail.com>
2018-05-12 22:41:49 -0600

    Got a PBR shader to render a sprite correctly using the default quad mesh

---
+++ FactoryReset/Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Transforms2D;
+using UnityEngine;
+
+public class Bootstrap
+{
+    public EntityArchetype PlayerArchetype { get; private set; }
+
+    public static Bootstrap Instance { get; } = new Bootstrap();
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void InitializeWorld()
+    {
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        Instance.PlayerArchetype = entityManager.CreateArchetype(typeof(TransformMatrix), typeof(Position2D));
+
+        Instance.GenerateSpriteMeshes();
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void AutoStart()
+    {
+        Instance.StartNewGame();
+    }
+
+    public void StartNewGame()
+    {
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        Entity player = entityManager.CreateEntity(PlayerArchetype);
+    }
+
+    void GenerateSpriteMeshes()
+    {
+
+    }
+}
